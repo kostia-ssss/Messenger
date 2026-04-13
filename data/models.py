74f709fa7dfc566c45414101c414ba65a6f866d5
kostia-ssss <kostiantyn.tel@gmail.com>
2026-04-13 15:40:12 +0300

    fixed build button

---
+++ data/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./store.db"
+engine = create_engine(
+ SQLALCHEMY_DATABASE_URL,
+connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False,
+autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String, index=True)
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(Integer, index=True)
+    receiver_id = Column(Integer, index=True)
+    content = Column(String, index=True)
+
+class Chat(Base):
+    __tablename__ = "chats"
+    id = Column(Integer, primary_key=True, index=True)
+    user1_id = Column(Integer, index=True)
+    user2_id = Column(Integer, index=True)
